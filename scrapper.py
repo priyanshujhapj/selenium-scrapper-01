d3cdaca0453aef3e73e60f725c046a132e50ad65
priyanshu87694 <priyanshu87694@gmail.com>
2023-07-03 00:49:01 +0530

    modifying scrapper for chromedriver-py

--- scrapper.py
+++ scrapper.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import json
 import os
 # import chromedriver_binary
-from get_chrome_driver import GetChromeDriver
+# from get_chrome_driver import GetChromeDriver
+from chromedriver_py import binary_path 
 
 
 basic_url = "https://www.finnomena.com"
@@ -71,20 +72,25 @@ def dump_data(names, links, grouped_list, file_name):
 def main(page, size):
 #   ### Chromedriver settings ###
 
-    # # Set up ChromeDriver path
+#   ### for chromedriver binary ###
     # chrome_driver_path = chromedriver_binary.chromedriver_filename
+
+#   ### for get chromedriver ###
+    # get_driver = GetChromeDriver()
+    # get_driver.install()
+    # driver = webdriver.Chrome()
+    
+#   ### for chromedriver-py ###
+    service = Service(binary_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
     # # Set Chrome options
     # chrome_options = Options()
-    # chrome_options.add_argument('--headless')  # Run ChromeDriver in headless mode
+    # chrome_options.add_argument('--headless')
 
     # # Create a new ChromeDriver instance
     # service = Service(chrome_driver_path)
     # driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    get_driver = GetChromeDriver()
-    get_driver.install()
-
-    driver = webdriver.Chrome()
 
     # Create new directory for output files
     os.mkdir('output')
