55021d2953f569b0f17a218b9264713feeded8f5
priyanshu87694 <priyanshu87694@gmail.com>
2023-07-02 23:54:41 +0530

    Restored flask verion 2.3.2 added print statements to app

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def custom_hello():
+    print("custom_hello()")
     return Response("{'RES-STATUS': 'OK-200'}", status=200)
 
 @app.route('/scrape', methods=['GET'])
 def scrape_data():
+    print("scrape_data() triggered")
     page = request.args['page']
+    print(f'PAGE:- {page}')
     size = request.args['size']
+    print(f'SIZE:- {size}')
     token = request.args['token']
+    print(f'TOKE:- {token}')
     if token == "624d1ef9e048c1869166ddc8":
         main(int(page), int(size))
         json_object = merge_files(int(page))
